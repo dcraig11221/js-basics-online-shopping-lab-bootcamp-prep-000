a6dc329c1433ec556cc8e8319af9333338d0a5ed
Douglas Craig <dcraig112@gmail.com>
2019-03-24 18:21:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,49 +19,11 @@ function total() {
 }
 
 function addToCart(item) {
-  let price = Math.floor(Math.random() * 100)
-  let o = {}
+      let price = Math.floor(Math.random() * 100)
+let o = {}
   o[item] = price
   cart.push(o)
   console.log(`${item} has been added to your cart.`)
   return cart
 }
 
-function viewCart() {
-  let myCart = []
-  if (cart.length > 0) {
-    for (let i = 0; i < cart.length; i++) {
-      let o = cart[i]
-      for (var key in o) {
-        myCart.push(` ${key} at $${o[key]}`)
-      }
-    }
-    console.log(`In your cart, you have${myCart}.`)
-    return cart
-  } else {
-    console.log(`Your shopping cart is empty.`)
-  }
-}
-
-function removeFromCart(item) {
-  for (let i = 0; i < cart.length; i++) {
-    let o = cart[i]
-    if (o.hasOwnProperty(item)) {
-      cart.splice(i, 1)
-      return cart
-    }
-  }
-  console.log(`That item is not in your cart.`)
-}
-
-function placeOrder(ccn) {
-  if (ccn) {
-    console.log(
-      `Your total cost is $${total()}, which will be charged to the card ${ccn}.`
-    )
-    cart = []
-  } else {
-    console.log(
-      `We don't have a credit card on file for you to place your order.`)
-  }
-}
